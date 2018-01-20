6f6fb122d96daea0439e8eb6005d5187377a588b
sheidaei <01@sheidaei.com>
2018-01-19 22:07:22 -0500

    Update dorzan.js

--- dorzan.js
+++ dorzan.js
@@ -3,7 +3,7 @@ var http = require('http'),
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({target:'http://localhost:'+PORT1}).listen(PORT2); // See (†)
+httpProxy.createProxyServer({target:'http://localhost:'+process.env.PORT}).listen(8000); // See (†)
 
 //
 // Create your target server
@@ -12,4 +12,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
-}).listen(PORT1);
+}).listen(process.env.PORT);
