c7e02acd1d7256de7de7e8890241fbc0a611992c
sheidaei <01@sheidaei.com>
2018-01-19 22:09:32 -0500

    Update dorzan.js

--- dorzan.js
+++ dorzan.js
@@ -1,15 +1,2 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-//
-// Create your proxy server and set the target in the options.
-//
-httpProxy.createProxyServer({target:'http://localhost:'+process.env.PORT}).listen(8000); // See (†)
-
-//
-// Create your target server
-//
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(process.env.PORT);
+var httpProxy = require('http-proxy');
+httpProxy.createServer(80, 'dorzan.herokuapp.com').listen(process.env.PORT || 8000);
